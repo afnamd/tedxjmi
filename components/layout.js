13ec71cf4f32096242009aa82184eed570278041
alhaqhassan <alhaqhassan@gmail.com>
2021-11-10 14:07:32 +0530

    added links && fixies

--- components/layout.js
+++ components/layout.js
@@ -5,9 +5,9 @@ const Layout = ({children}) =>{
     return (
         <div className="Layout ">
           <Head>
-            <title>TEDxJMI 2022</title>
+            <title>TEDx JMI 2022 | Jamia Millia Islamia</title>
             <meta name="description" content="TEDxJMI, x = independently organised TED event. Different Lenses. This January, at Jamia Millia Islamia." />
-            <meta name="keywords" content="TEDx, JMI, Jamia Millia Islamia, New Delhi, event, talks"/>
+            <meta name="keywords" content="tedxjmi, TEDx, JMI, Jamia Millia Islamia, New Delhi, event, talks"/>
           </Head>
           <div className=' ml-auto mr-auto bg-black'>
             <Navbar />
--- pages/homepage/partners.js
+++ pages/homepage/partners.js
@@ -1,28 +1,26 @@
 import React from "react";
+import AfnamdLogo from "../../components/svg/afnamd";
 const Partners = () =>{
     return(
-        <div class="bg-white rounded-2xl p-12 flex flex-col gap-12">
+        <div class="bg-white rounded-2xl p-12 flex flex-col gap-12 min-h-400">
             <div class="font-Cinzel text-2xl">
             <div class=" text-center">
-                Partners
+                <h3>Partners</h3>
             </div>
             <div class="flex  justify-around mt-5">
                 <div>
-                afnamd
-                </div>
-                <div>
-                afnamd
+                   <AfnamdLogo fill={'#2A2829'} w={256}/>
                 </div>
             </div>
             </div>
         
             <div class="font-Cinzel text-2xl">
-            <div class=" text-center">
+            {/* <div class=" text-center">
                 Archive
             </div>
             <div class="flex  justify-around mt-5">
                 *---------------------*----*----*
-            </div>
+            </div> */}
             </div>
         </div>
     )
