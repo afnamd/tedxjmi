97d9d5d868cc30176b20af5a7b7f6321f142941d
Husain Shahid Rao <husain3012@pop-os.localdomain>
2021-11-10 13:51:18 +0530

    add partners

--- pages/homepage/partners.js
+++ pages/homepage/partners.js
@@ -6,13 +6,19 @@ const Partners = () =>{
             <div class=" text-center">
                 Partners
             </div>
-            <div class="flex  justify-around mt-5">
-                <div>
-                afnamd
+            <div className="flex flex-wrap w-full items-center justify-around mt-5">
+                <div >
+                    <img className="w-44" src="/static/images/afnamd-logo.png" alt="afnamd" />
                 </div>
                 <div>
-                afnamd
+                    <img className="w-44" src="/static/images/lotto-logo.png" alt="afnamd" />
                 </div>
+                {/* <div>
+                    <img className="w-44" src="/static/images/lotto-logo.png" alt="afnamd" />
+                </div>
+                <div>
+                    <img className="w-44" src="/static/images/lotto-logo.png" alt="afnamd" />
+                </div> */}
             </div>
             </div>
         
