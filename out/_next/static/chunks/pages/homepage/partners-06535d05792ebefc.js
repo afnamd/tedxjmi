ca2289b40cdc0784ef937e8d62584cd1b12213cc
alhaqhassan <alhaqhassan@gmail.com>
2021-11-11 10:07:52 +0530

    Deploy gh-pages

--- out/_next/static/chunks/pages/homepage/partners-06535d05792ebefc.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[900],{5145:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/homepage/partners",function(){return u(5169)}])}},function(n){n.O(0,[852,169,774,888,179],(function(){return _=5145,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
