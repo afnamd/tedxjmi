250d74647fd07fa7aa1eb8587acdbfb4f3b7539a
alhaqhassan <alhaqhassan@gmail.com>
2022-01-18 22:56:40 +0530

    Updates

---
+++ archive/breaking-shackles/precache-manifest.bc00648d03e0120459db6ad2b247f7b5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b71b1ed93493015faf586658e54948f3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "23c7567db978854bc9ef",
+    "url": "/static/css/main.ebeafff7.chunk.css"
+  },
+  {
+    "revision": "313a7d01018d6254dc97",
+    "url": "/static/js/2.4c5e2669.chunk.js"
+  },
+  {
+    "revision": "23c7567db978854bc9ef",
+    "url": "/static/js/main.690f8fe4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
