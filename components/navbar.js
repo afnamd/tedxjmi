00d4362534333aa9a6bdb7d50e02d1414a77c60f
Mohammad Sarfraz Alam <mohammadsarfrazalam@pop-os.localdomain>
2022-01-20 12:39:20 +0530

    Package

--- components/navbar.js
+++ components/navbar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Link from "next/link";
 const Navbar = () => {
   return (
     <div className="flex justify-evenly items-center p-4 bg-white">
@@ -6,9 +7,12 @@ const Navbar = () => {
         <div className="font-Cinzel p-4">About</div>
         <div className="font-Cinzel p-4">Speaker</div>
       </div>
-      <div>
-        <img src="https://i.ibb.co/ncZ8qLG/image.png" className="h-10 " />
+      <div className="cursor-pointer">
+        <Link href="/">
+          <img src="https://i.ibb.co/ncZ8qLG/image.png" className="h-10 " />
+        </Link>
       </div>
+
       <div className="md:flex hidden">
         <div className="font-Cinzel p-4">Archive</div>
         <div className="font-Cinzel p-4">Contact</div>
