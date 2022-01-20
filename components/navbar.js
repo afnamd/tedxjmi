656ef06f5d167dfa2e6217d1e5da63ac71c8a323
Sarfraz-droid <Sarf.alam0206>
2022-01-20 12:45:41 +0530

    Navbar

--- components/navbar.js
+++ components/navbar.js
@@ -4,18 +4,24 @@ const Navbar = () => {
   return (
     <div className="flex justify-evenly items-center p-4 bg-white">
       <div className="md:flex hidden">
-        <div className="font-Cinzel p-4">About</div>
-        <div className="font-Cinzel p-4">Speaker</div>
+          <Link href={"/archive/2021"}>
+            <div className="font-Cinzel p-4">About</div>
+          </Link>
+          <Link href={"/archive/2021"}>
+            <div className="font-Cinzel p-4">Speaker</div>
+          </Link>
       </div>
       <div className="cursor-pointer">
-        <Link href="/">
-          <img src="https://i.ibb.co/ncZ8qLG/image.png" className="h-10 " />
-        </Link>
+          <Link href="/">
+              <img src="https://i.ibb.co/ncZ8qLG/image.png" className="h-10 " />
+          </Link>
       </div>
 
       <div className="md:flex hidden">
-        <div className="font-Cinzel p-4">Archive</div>
-        <div className="font-Cinzel p-4">Contact</div>
+          <Link href={"/archive/2011"}>
+            <div className="font-Cinzel p-4">Archive</div>
+          </Link>
+          <div className="font-Cinzel p-4">Contact</div>
       </div>
     </div>
   );
