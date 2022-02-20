7ee419fe16292c0c1d306ca312196cad32836be1
Sarfraz-droid <Sarf.alam0206>
2022-02-20 23:34:42 +0530

    screen fix

--- api/auth.js
+++ api/auth.js
@@ -1,29 +1,29 @@
 import xhr from './xhr'
 const config = [{
-	headers: {
-		"Content-Type": "application/x-www-form-urlencoded"
-	},
-	withCredentials: true
+    headers: {
+        "Content-Type": "application/x-www-form-urlencoded"
+    },
+    withCredentials: true
 }]
-class auth{
-    static handshake = async() =>{
-        try{
-            let res = await xhr.get(`${process.env.NEXT_PUBLIC_HOST}/auth/handshake`, {withCredentials: true});
+class auth {
+    static handshake = async () => {
+        try {
+            let res = await xhr.get(`${process.env.NEXT_PUBLIC_HOST}/auth/handshake`, { withCredentials: true });
             localStorage.setItem('csrf', res.data.csrf);
             return res
-        }catch(err){
-            console.log(err)
+        } catch (err) {
+            console.log(err);
             alert('something went wrong, please refresh the page')
         }
     }
-    static status = () =>{
+    static status = () => {
         return xhr.get(`${process.env.NEXT_PUBLIC_HOST}/auth/status`, ...config);
     }
-    static login = (token) =>{
-        return xhr.post(`${process.env.NEXT_PUBLIC_HOST}/auth/signin/`,  {token}, ...config)
+    static login = (token) => {
+        return xhr.post(`${process.env.NEXT_PUBLIC_HOST}/auth/signin/`, { token }, ...config)
     }
-    static logout = async() =>{
-        let res = await xhr.post(`${process.env.NEXT_PUBLIC_HOST}/auth/logout/`,  {}, ...config)
+    static logout = async () => {
+        let res = await xhr.post(`${process.env.NEXT_PUBLIC_HOST}/auth/logout/`, {}, ...config)
         this.handshake()
         return res
     }
--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,35 @@
+import React, { useEffect } from 'react'
 import 'tailwindcss/tailwind.css'
 import '../styles/style.css'
 
 import { RecoilRoot } from 'recoil'
 
 function MyApp({ Component, pageProps }) {
+
+  useEffect(() => {
+    var customViewportCorrectionVariable = 'vh';
+
+
+    function setViewportProperty(doc) {
+      var prevClientHeight;
+      var customVar = '--' + (customViewportCorrectionVariable || 'vh');
+      function handleResize() {
+        var clientHeight = doc.clientHeight;
+        if (clientHeight === prevClientHeight) return;
+        requestAnimationFrame(function updateViewportHeight() {
+          doc.style.setProperty(customVar, (clientHeight * 0.01) + 'px');
+          prevClientHeight = clientHeight;
+        });
+      }
+      handleResize();
+      return handleResize;
+    }
+    window.addEventListener('resize', setViewportProperty(document.documentElement));
+
+    return (() => {
+      window.removeEventListener('resize', setViewportProperty(document.documentElement));
+    })
+  }, [])
 
   return (
     <RecoilRoot>
