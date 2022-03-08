385a7fffa9daeac0213c3b7dab88cbedd4ab7264
alhaqhassan <alhaqhassan@gmail.com>
2022-03-08 22:23:55 +0530

    Updates

---
+++ _next/static/chunks/pages/homepage/tickets-263319c35183f156.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[358],{5613:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/homepage/tickets",function(){return u(7079)}])}},function(n){n.O(0,[782,79,774,888,179],(function(){return _=5613,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
